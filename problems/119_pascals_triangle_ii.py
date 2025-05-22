4ef4a0ae214b9b39851b114e460c98c604aac94f
soif2sang <sirefight.gaming@gmail.com>
2025-05-22 15:10:47 +0200

    Solve(119): Final Answer

---
+++ problems/119_pascals_triangle_ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        output = []
+        for i in range(1, rowIndex + 2):
+            if i == 1:
+                output.append([1])
+            elif i == 2:
+                output.append([1, 1])
+            else:
+                tmp = [1]
+                for y in range(1, i - 1):
+                    tmp.append(output[i - 2][y - 1] + output[i - 2][y])
+                tmp.append(1)
+
+                output.append(tmp)
+
+        return output[rowIndex]
